4dcac49c8f7d770de92d5b033bf10e163ad6d6ea
Gustavo Sampaio <gbritosampaio@gmail.com>
2017-02-08 22:45:50 -0300

    New date

--- js/data.service.js
+++ js/data.service.js
@@ -7,7 +7,7 @@ angular.module('spah')
     palestras: [
       {
         nome: 'A definir',
-        data_hora: new Date(2017, 1, 10, 15, 0),
+        data_hora: new Date(2017, 4, 2, 19, 0),
         palestrante: {
           nome: 'Dr. Sérgio Araújo',
           cv: '',
@@ -16,7 +16,7 @@ angular.module('spah')
       },
       {
         nome: 'Segurança do Paciente e Implicações Jurídicas',
-        data_hora: new Date(2017, 1, 6, 11, 0),
+        data_hora: new Date(2017, 4, 2, 19, 0),
         palestrante: {
           nome: 'Dra. Lívia Siebra',
           cv: 'Advogada inscrita na OAB/CE 28.897. Graduada pela Universidade Regional do Cariri. Pós-graduada em Direito Penal e Criminologia pela URCA. Pós-graduanda em Direito Médico, Hospitalar e da Saúde pela ESMECAM. Especialista em Direito Médico/Hospitalar e da Saúde.',
@@ -34,7 +34,7 @@ angular.module('spah')
       },
       {
         nome: 'A definir',
-        data_hora: new Date(2017, 2, 10, 15, 0),
+        data_hora: new Date(2017, 2, 1, 15, 0),
         palestrante: {
           nome: 'A definir',
           cv: '',
