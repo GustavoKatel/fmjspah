99328b2589341766669a35905fc9ce986efceea4
Gustavo Sampaio <gbritosampaio@gmail.com>
2017-04-08 13:59:23 -0300

    Update palestra date, times and cv's

--- js/data.service.js
+++ js/data.service.js
@@ -7,17 +7,17 @@ angular.module('spah')
     palestras: [
       {
         nome: 'A definir',
-        data_hora: new Date(2017, 4, 2, 19, 0),
+        data_hora: new Date(2017, 4, 3, 19, 0),
         palestrante: {
           nome: 'Dr. Sérgio Araújo',
-          cv: '',
+          cv: 'Médico. Cirurgião do Aparelho Digestivo. Professor da Disciplina de Clínica Cirúrgica da Estácio FMJ. Coordenador do internato Médico da Estácio FMJ. Pós graduado em Gestão de qualidade pela ESPCE e Sírio Libanês. COordenador do I Curso de Segurança do Paciente em Ambiente Hospitalar. Coordenador do I Simpósio de Segurança do Paciente em Ambiente Hospitalar e do II Curso de Segurança do Paciente em Ambiente Hospitalar.',
           foto: '/images/palestrantes/dr.sergio.jpeg'
         }
       },
 
       {
         nome: 'Segurança do Paciente e Implicações Jurídicas',
-        data_hora: new Date(2017, 4, 2, 19, 0),
+        data_hora: new Date(2017, 4, 2, 21, 20),
         palestrante: {
           nome: 'Dra. Lívia Siebra',
           cv: 'Advogada inscrita na OAB/CE 28.897. Graduada pela Universidade Regional do Cariri. Pós-graduada em Direito Penal e Criminologia pela URCA. Pós-graduanda em Direito Médico, Hospitalar e da Saúde pela ESMECAM. Especialista em Direito Médico/Hospitalar e da Saúde.',
@@ -27,7 +27,7 @@ angular.module('spah')
 
       {
         nome: 'Manejo seguro dos Medicamentos – Da prescrição à conciliação',
-        data_hora: new Date(2017, 4, 5, 21, 30),
+        data_hora: new Date(2017, 4, 6, 9, 30),
         palestrante: {
           nome: 'Rosangela Maria Araújo Teles',
           cv: 'Farmacêutica, gerente da Farmacia do Hospital Regional do Cariri- ISGH, pós-graduanda no curso de Especializaçao em Atençao Farmaceutica',
@@ -37,7 +37,7 @@ angular.module('spah')
 
       {
         nome: 'A definir',
-        data_hora: new Date(2017, 4, 3, 15, 0),
+        data_hora: new Date(2017, 4, 6, 8, 50),
         palestrante: {
           nome: 'Rodrigo Luis Mousinho Gomes',
           cv: 'Formação em Fisioterapia pela UFPE, Pós em UTI e Fisioterapia Cardiorrespiratória pela UFPE, Pós docência do Ensino Superios UNESA, Mestrado em Ciências da Saúde pela UFPE, Fisioterapeuta plantonista da UTI do Hospital São Vicente e Professor Coordenador do curso de Fisioterapia Estácio FMJ',
@@ -47,7 +47,7 @@ angular.module('spah')
 
       {
         nome: 'Manejo de Hemocomponentes - Fazendo Hemotransfusão Segura',
-        data_hora: new Date(2017, 4, 5, 19, 40),
+        data_hora: new Date(2017, 4, 5, 19, 50),
         palestrante: {
           nome: 'Christianne Andrezza Melo Sobreira',
           cv: 'Mestre em Saúde Coletiva com concentração em Gestão em Saúde e ênfase em hemoterapia. Coordenadora de enfermagem da Agência Transfusional do Hospital Regional do Cariri. Avaliadora do Programa Nacional de Qualificação da Hemorrede.',
@@ -67,7 +67,7 @@ angular.module('spah')
 
       {
         nome: 'Pneumonia relacionada à Ventilação Mecânica - Como prevenir?',
-        data_hora: new Date(2017, 4, 6, 20, 40),
+        data_hora: new Date(2017, 4, 4, 19, 00),
         palestrante: {
           nome: 'Thiago Alcanfor',
           cv: 'Pós graduado em Fisioterapia em Terapia Intensiva pela Faculdade Redentor - Rio de Janeiro - RJ. Título de Especialista em Fisioterapia em Terapia Intensiva pela Associação Brasileira de Fisioterapia Cardiovascular, Respiratória e Fisioterapia em Terapia Intensiva - ASSOBRAFIR. Diretor da Regional Ceará da ASSOBRAFIR. Coordenador da Equipe de Fisioterapia do Hospital Regional do Cariri - HRC. Delegado do Conselho de Regional de Fisioterapia e Terapia Ocupacional da 6\' Região - CREFITO6. Docente de Fisioterapia em Terapia Intensiva da Faculdade de Integração do Sertão - FIS - Serra Talhada - PE',
@@ -97,7 +97,7 @@ angular.module('spah')
 
       {
         nome: 'A definir',
-        data_hora: new Date(2017, 4, 2, 20, 0),
+        data_hora: new Date(2017, 4, 4, 19, 0),
         palestrante: {
           nome: 'Monize de Freitas Neuron',
           cv: 'Enfermeira Formada pela Universidade Regional do Cariri - Campus Avançado de Iguatu CAI. Em 2010. Possui Pós Graduação em Pesquisa e Inovação em Saúde da Família Pela UFC. Enfermeira Socorrista com Curso de APH. Foi Enfermeira Assistencialista em diversos hospitais da região centro sul do estado .Ataundo ainda como Docente e na Preceptoria de estágios da URCA no Curso de Enfermagem.Atualmente sou Coordenadora de Enfermagem Da Unidade de Cuidados Especiais (UCE) e da Unidade de AVC (UAVC) do Hospital Regional do Cariri.',
@@ -107,7 +107,7 @@ angular.module('spah')
 
       {
         nome: 'A definir',
-        data_hora: new Date(2017, 4, 2, 20, 0),
+        data_hora: new Date(2017, 4, 3, 20, 0),
         palestrante: {
           nome: 'Nárya Maria Gonçalves de Brito',
           cv: 'Gerente de Risco do Hospital Regional do Cariri. Enfermeira do Hospital Regional de Cajazeiras. Especialista em Gestão da Qualidade no Ambiente Hospitalar. Mestranda em Ciências da Saúde.',
